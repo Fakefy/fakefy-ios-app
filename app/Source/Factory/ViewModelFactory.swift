13f49215f1b5f89ebbfeb6013dc6eaf9a70b0fb4
neymoura <ney.pequeno@greenlight.me>
2022-06-06 08:19:04 -0300

    Adding two aditionals ways to create the SearchViewModel in the ViewModelFactory

--- app/Source/Factory/ViewModelFactory.swift
+++ app/Source/Factory/ViewModelFactory.swift
@@ -9,6 +9,15 @@ import FakefyDomain
 import FakefyData
 
 class ViewModelFactory {
+    static func makeSearchViewModel(repository: SearchRepository) -> SearchViewModel {
+        let useCase = UseCaseFactory.makeSearchUseCase(searchRepository: repository)
+        return SearchViewModelImpl(useCase: useCase)
+    }
+    
+    static func makeSearchViewModel(useCase: SearchAlbumUseCase) -> SearchViewModel {
+        return SearchViewModelImpl(useCase: useCase)
+    }
+    
     static func makeSearchViewModel() -> SearchViewModel {
         let repository = RepositoryFactory.makeSearchRepository()
         let useCase = UseCaseFactory.makeSearchUseCase(searchRepository: repository)
